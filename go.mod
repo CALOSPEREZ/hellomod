f67d323383f51f126b84b495b337f89fd7149b33
Carlos perez Alberto Abreu <dcyt2019@gmail.com>
2022-05-06 06:15:02 -0400

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CALOSPEREZ/hellomod
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,12 @@
+package utils
+
+import "fmt"
+
+func HelloWord() {
+	fmt.Println("dmo")
+}
+
+func sum(x int, y int) (value int) {
+	value = x + y
+	return
+}
